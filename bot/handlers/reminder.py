d7e8d2ee04db44491c3ed8022845a2e0748e8cfd
Daniella <121246750+slaaay@users.noreply.github.com>
2024-02-29 03:36:48 +0200

    Update reminder.py

--- bot/handlers/reminder.py
+++ bot/handlers/reminder.py
@@ -1,7 +1,7 @@
 import logging
 
-from bot.db import list_today_events, get_all_chat_ids
-from bot.bot_setup import scheduler
+from bot.db import DatabaseManager
+from bot.bot_setup import scheduler, WAY
 
 from config import DEFAULT_TIMEZONE
 from datetime import datetime, timedelta
@@ -12,17 +12,19 @@ from apscheduler.triggers.cron import CronTrigger
 
 scheduler.configure(timezone=DEFAULT_TIMEZONE)
 
+db_manager = DatabaseManager(WAY)
+
 # Функція, яку ми хочемо виконувати щодня
 async def daily_reminder():
     from bot.bot_setup import bot
     logging.info("Daily reminder function is called")
     
-    chat_ids = get_all_chat_ids()  # Функція, яка отримує всі chat_id з бази даних
+    chat_ids = await db_manager.get_all_chat_ids()  # Функція, яка отримує всі chat_id з бази даних
 
     for chat_id in chat_ids:
         try:
             # Отримуємо список подій для конкретного chat_id
-            events = list_today_events()
+            events = await db_manager.list_today_events()
             for event in events:
                 # Надсилаємо повідомлення
                 await bot.send_message(chat_id, f"{hbold("Назва:")} {event[1]} \n{hbold("Площа:")} {event[2]} \n{hbold("Дія:")} {event[3]} \n{hbold("Дата:")} {event[4]} {event[5]}")
@@ -44,9 +46,9 @@ def schedule_send_reminder(scheduler, chat_id, event_id, name, venue, action, ev
 
 async def send_reminder(chat_id, event_id, name, venue, action, event_datetime, bot):
     logging.info(f"Trying to send a reminder for event: {name}, chat_id: {chat_id}, event_id: {event_id}")
-    chat_ids = get_all_chat_ids()  # Получаем список всех chat_id
+    chat_ids = await db_manager.get_all_chat_ids()  # Получаем список всех chat_id
 
-    # Припускаючи, що 'event_datetime' вже в правильному часовому поясі, конвертуємо у формат без часового поясу для виводу
+    # Конвертуємо у формат без часового поясу для виводу
     naive_datetime = event_datetime.replace(tzinfo=None)
     formatted_datetime = naive_datetime.strftime('%Y-%m-%d %H:%M:%S')
     reminder_message = f"<b>Нагадування про подію!</b> \n\n<b>Назва:</b> {name} \n<b>Площа:</b> {venue} \n<b>Дія:</b> {action} \n<b>Дата:</b> {formatted_datetime} UTC"
